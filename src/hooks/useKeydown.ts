9c6b925d4b44575703bf114044bfc1435cbf4cb3
haikelz <yuukisakura@proton.me>
2022-10-17 20:17:39 +0700

    feat: add useKeydown hook

--- src/hooks/useKeydown.ts
+++ src/hooks/useKeydown.ts
@@ -1,25 +1,27 @@
-import { MutableRefObject, useEffect } from "react";
+import { MutableRefObject, useCallback, useEffect } from "react";
 import { KeydownEvent } from "src/interfaces";
 
-// ref, isShiftKey, key1, key2
 export const useKeydown = <T extends MutableRefObject<string | any>>(
   ref: T,
   isShiftKey: boolean,
   key1: string,
   key2: string
 ) => {
-  const handleKeydown = <T extends KeydownEvent>(event: KeydownEvent) => {
-    if (event.shiftKey === isShiftKey && event.key === key1) {
-      ref.current?.focus();
-    }
+  const handleKeydown = useCallback(
+    <T extends KeydownEvent>(event: T) => {
+      if (event.shiftKey === isShiftKey && event.key === key1) {
+        ref.current?.focus();
+      }
 
-    if (event.key === key2) {
-      ref.current?.blur();
-    }
-  };
+      if (event.key === key2) {
+        ref.current?.blur();
+      }
+    },
+    [ref, isShiftKey, key1, key2]
+  );
 
   useEffect(() => {
-    document.addEventListener("keydown", handleKeydown);
+    document.addEventListener("keydown", handleKeydown, true);
     return () => document.removeEventListener("keydown", handleKeydown);
-  }, []);
+  }, [handleKeydown]);
 };
